b0ede8e2fa2ea4fd0c812dedfc82a436d32f1bc2
GabrielDom <gabrieldominguez@retargetly.com>
2024-09-09 17:51:23 -0300

    style: forecast card modified

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -22,26 +22,64 @@ const Forecast: React.FC<ForecastProps> = ({ city }) => {
         display="flex"
         justifyContent="center"
         mt={2}
+        gap={2}
         flexWrap="wrap"
         sx={{
-          flexDirection: { xs: "column", sm: "row" },
+          flexDirection: {
+            xs: "column",
+            sm: "row",
+            md: "column",
+            lg: "column",
+            xl: "column",
+          },
           alignItems: { xs: "center", sm: "flex-start" },
         }}
       >
         {data.forecast.forecastday.map((day: ForecastDay) => (
           <Card
             key={day.date}
-            sx={{ margin: { xs: "10px 0", sm: "0 10px" }, width: 200 }}
+            sx={{
+              margin: { xs: "10px 0", sm: "0 10px" },
+              width: { xs: 300, sm: 300, md: 300, lg: 350, xl: 350 },
+              display: { xs: "flex" },
+            }}
           >
-            <CardContent>
-              <Typography variant="h6">{day.date}</Typography>
-              <img src={day.day.condition.icon} alt={day.day.condition.text} />
-              <Typography variant="body1">{day.day.condition.text}</Typography>
-              <Typography variant="body2">
-                Máx: {day.day.maxtemp_c}°C
+            <CardContent
+              sx={{
+                display: { xs: "flex" },
+                alignItems: { xs: "center" },
+                justifyContent: { xs: "center" },
+                gap: { xs: 4 },
+              }}
+            >
+              <Typography variant="h6">
+                {new Date(day.date).toLocaleString("en-US", {
+                  weekday: "short",
+                })}
               </Typography>
+              <Box
+                sx={{
+                  width: { xs: "24px" },
+                  height: { xs: "24px" },
+                  display: { xs: "flex" },
+                  alignItems: { xs: "center" },
+                  justifyContent: { xs: "center" },
+                }}
+              >
+                <img
+                  src={day.day.condition.icon}
+                  alt={day.day.condition.text}
+                />
+              </Box>
+              <Typography variant="body1" sx={{ display: { xs: "none" } }}>
+                {day.day.condition.text}
+              </Typography>
+
               <Typography variant="body2">
                 Mín: {day.day.mintemp_c}°C
+              </Typography>
+              <Typography variant="body2">
+                Máx: {day.day.maxtemp_c}°C
               </Typography>
             </CardContent>
           </Card>
